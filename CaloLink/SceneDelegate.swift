3ea8c7e1682c9f7ba3ad2904c3f1255fc487890d
snughnu <ksnughnu@gmail.com>
2025-04-06 22:50:52 +0900

    chore: swiftlintPlugin 설정
    - .yml파일 작성
    - 프로젝트 설정
    - 앱델, 뷰컨, 씬델에서 린트규칙 적용

--- CaloLink/SceneDelegate.swift
+++ CaloLink/SceneDelegate.swift
@@ -11,8 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let navigationController = UINavigationController(rootViewController: ViewController())
@@ -49,7 +52,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
