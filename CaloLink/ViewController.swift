05b14ac768ddb8cd5c14227730681cddc9bef41d
snughnu <ksnughnu@gmail.com>
2025-04-06 18:49:59 +0900

    Initial commit
    - 프로젝트 생성
    - 스토리보드 삭제 세팅
    - .gitignore 작성

---
+++ CaloLink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaloLink
+//
+//  Created by 김성훈 on 4/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
