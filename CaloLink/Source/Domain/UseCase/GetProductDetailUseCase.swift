06a97e4b3a6fba9d15def007da7840a2ce65785d
snughnu <ksnughnu@gmail.com>
2025-08-12 19:30:14 +0900

    GetProductDetailUseCase 시그니처 작성

--- CaloLink/Source/Domain/UseCase/GetProductDetailUseCase.swift
+++ CaloLink/Source/Domain/UseCase/GetProductDetailUseCase.swift
@@ -5,3 +5,32 @@
 //  Created by 김성훈 on 8/12/25.
 //
 
+// MARK: - GetProductDetailUseCaseProtocol
+// 상품 상세 정보 조회 비즈니스 로직을 정의하는 프로토콜
+protocol GetProductDetailUseCaseProtocol {
+    func execute(
+        productId: String,
+        completion: @escaping (Result<ProductDetail, Error>) -> Void
+    )
+}
+
+// MARK: - GetProductDetailUseCase
+final class GetProductDetailUseCase: GetProductDetailUseCaseProtocol {
+    // 작업을 전달할 레포지토리 선언
+    private let productRepository: ProductRepositoryProtocol
+
+    // 의존성 주입(DI)을 통해 Repository를 받음
+    init(productRepository: ProductRepositoryProtocol) {
+        self.productRepository = productRepository
+    }
+
+    // UseCase의 핵심 로직을 수행하는 메서드
+    func execute(
+        productId: String,
+        completion: @escaping (Result<ProductDetail, Error>) -> Void
+    ) {
+        // TODO: - 실제 구현은 Data Layer가 완성된 후 작성
+        // 지금은 Repository에 작업을 그대로 전달하는 역할만 함
+        productRepository.fetchProductDetail(productId: productId, completion: completion)
+    }
+}
