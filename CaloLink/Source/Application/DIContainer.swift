f233fcbd3d6744cd0cbb86497feff97da0363318
snughnu <ksnughnu@gmail.com>
2025-08-13 09:28:31 +0900

    DIContainer 작성
    팩토리 패턴 사용

---
+++ CaloLink/Source/Application/DIContainer.swift
@@ -0,0 +1,56 @@
+//
+//  DIContainer.swift
+//  CaloLink
+//
+//  Created by 김성훈 on 8/13/25.
+//
+
+import Foundation
+
+// MARK: - DIContainer
+// 앱의 모든 의존성을 생성하고 관리하는 중앙 컨테이너
+final class DIContainer {
+    // MARK: - 부품 창고 (Singleton Instances)
+
+    // Services: 네트워크 통신 엔진은 앱 전체에서 하나만 존재
+    lazy var networkService: NetworkServiceProtocol = DefaultNetworkService()
+
+    // Repositories: 데이터 저장소도 하나만 존재
+    lazy var productRepository: ProductRepositoryProtocol = MockProductRepository()
+//    lazy var productRepository: ProductRepositoryProtocol = DefaultProductRepository(networkService: networkService)
+
+    // Use Cases: UseCase는 상태를 가지지 않으므로 하나만 만들어 재사용
+    lazy var searchProductsUseCase: SearchProductsUseCaseProtocol = SearchProductsUseCase(productRepository: productRepository)
+    lazy var getProductDetailUseCase: GetProductDetailUseCaseProtocol = GetProductDetailUseCase(productRepository: productRepository)
+
+/*
+    // MARK: - 조립 라인 (Factory Methods)
+
+    // MARK: - 검색 결과 목록 Scene
+    // ListViewModel을 생성하는 팩토리 메서드
+    func makeListViewModel() -> ListViewModel {
+        // 부품 창고에서 "searchProductsUseCase"를 가져와 주입
+        return ListViewModel(searchProductsUseCase: searchProductsUseCase)
+    }
+
+    // ListViewController를 생성하는 팩토리 메서드
+    func makeListViewController() -> ListViewController {
+        // "makeListViewModel" 조립 라인을 호출하여 새로운 ViewModel을 만들고 주입
+        return ListViewController(viewModel: makeListViewModel())
+    }
+
+    // MARK: - 상품 상세 Scene
+    // DetailViewModel을 생성하는 팩토리 메서드
+    func makeDetailViewModel(productId: String) -> DetailViewModel {
+        return DetailViewModel(
+            productId: productId,
+            getProductDetailUseCase: getProductDetailUseCase
+        )
+    }
+
+    // DetailViewController를 생성하는 팩토리 메서드
+    func makeDetailViewController(productId: String) -> DetailViewController {
+        return DetailViewController(viewModel: makeDetailViewModel(productId: productId))
+    }
+*/
+}
